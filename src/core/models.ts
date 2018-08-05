3f81e06ff03d33ff036b391b35cf77e31738b9f1
Jeremy Farnault <haskkor@gmail.com>
2018-08-06 09:05:04 +1200

    Started to work on the heroes creation

--- src/core/models.ts
+++ src/core/models.ts
@@ -1,3 +1,5 @@
+// Tiles
+
 export interface ITile {
   uuid: string
   columnIndex: number
@@ -13,4 +15,51 @@ export enum TileState {
 
 export enum TileColors {
   empty = 'orange'
+}
+
+// Heroes
+
+export interface IHeroes {
+  [id: string]: IHero
+}
+
+export interface IHero {
+  id: string
+  characteristics: IHeroCharacteristics
+  cost: number
+  unique: boolean
+  skills: string[]
+  description: string
+  assets: IHeroAssets
+}
+
+export interface IHeroCharacteristics {
+  speed: number
+  initiative: number
+  lifePoints: number
+  magicPoints: number
+  armor: number
+  range: number
+  size: HeroSize
+  agility: number
+  stamina: number
+  strength: number
+  magic: number
+  mental: number
+  attack: number
+  rangedAttack: number
+  attackArmor: number
+}
+
+export interface IHeroAssets {
+  tokenPath: string
+  tokenInitPath: string
+  miniaturePath: string
+  battlefieldPath: string
+}
+
+export enum HeroSize {
+  small = 'S',
+  medium = 'M',
+  large = 'L'
 }
