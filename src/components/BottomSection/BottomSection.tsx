fbab1645823c9053208f46b116e1ae41947f918f
Jeremy Farnault <haskkor@gmail.com>
2018-11-19 09:11:34 +1300

    Started to work on the modal

--- src/components/BottomSection/BottomSection.tsx
+++ src/components/BottomSection/BottomSection.tsx
@@ -1,10 +1,12 @@
 import { ActionsType, IHeroBattlefield, VariousAssets } from "../../core/models";
+import { colors } from "../../utils/colors";
 import ActionButtons from "../ActionButtons/ActionButtons";
 import InitiativeAndDetails from "../InitiativeAndDetails/InitiativeAndDetails";
 import Timer from "../Timer/Timer";
 import { BottomSectionContainer, DeckImage, LeftSection, RightSection } from "./BottomSectionStyles";
 
 import * as React from "react";
+import * as Modal from "react-modal";
 
 interface IProps {
   heroesSorted: IHeroBattlefield[]
@@ -15,9 +17,39 @@ interface IProps {
   changeAction: (action: ActionsType) => void
 }
 
-// interface IState {}
+interface IState {
+  modalOpen: boolean
+}
 
-class BottomSection extends React.PureComponent<IProps, {}> {
+const customStyles = {
+  overlay: {
+    zIndex: 10,
+    backgroundColor: colors.blackHighOpacity
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "black",
+    padding: 20,
+    borderSize: 2,
+    borderStyle: "solid",
+    borderColor: colors.paleYellow
+  }
+};
+
+class BottomSection extends React.PureComponent<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
+
 
   public render() {
     const heroes = this.props.heroesSorted;
@@ -39,11 +71,25 @@ class BottomSection extends React.PureComponent<IProps, {}> {
         <RightSection>
           <Timer hero={heroes[0]} minutesTurn={1} secondsTurn={0}
                  nextHero={this.props.setNextCurrentHero}/>
-          <DeckImage src={VariousAssets.cardBack.path}/>
+          <DeckImage src={VariousAssets.cardBack.path} onClick={this.openModal}/>
         </RightSection>
+        <Modal
+          style={customStyles}
+          isOpen={this.state.modalOpen}
+          onRequestClose={this.closeModal}>
+          <div>TEST</div>
+        </Modal>
       </BottomSectionContainer>
     );
   }
+
+  private openModal = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  private closeModal = () => {
+    this.setState({ modalOpen: false });
+  };
 }
 
 export default BottomSection;
