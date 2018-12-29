874ede2b14c43ff1e70257b94c5a5018ecd358d1
Jérémy Farnault <haskkor@gmail.com>
2018-12-29 21:09:41 +1300

    Start working on hero colors

--- src/components/Hero/HeroStyles.tsx
+++ src/components/Hero/HeroStyles.tsx
@@ -9,7 +9,7 @@ position: absolute;
 top: ${(props: IStyledHero) => props.posY}px;
 left: ${(props: IStyledHero) => props.posX}px;
 filter:drop-shadow(2px 2px 0 ${(props: IStyledHero) => props.selected ? colors.red : colors.transparent})
-drop-shadow(-2px -2px 0 ${(props: IStyledHero) => props.selected ? colors.red : colors.transparent});
+drop-shadow(-2px -2px 0 ${(props: IStyledHero) => props.shadowColor});
 &:hover {
 opacity: ${grid.opacity};
 }
